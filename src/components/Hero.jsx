6be043ff2cfabed15fc5b8b32187e677fe92a9fe
Joshua Garvey <j.garvey514@yahoo.com>
2023-06-27 01:06:39 -0400

    627

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
 			className={`relative w-full h-screen mx-auto bg-black
 		`}
 		>
-			<div className={`${styles.paddingX} absolute inset-0 top-[340px] md:top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+			<div className={`${styles.paddingX} absolute inset-0 top-[440px] md:top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
 				<div className="flex flex-col justify-center items-center mt-5">
 					<div className="w-5 h-5 rounded-full bg-[#686C45]" />
 					<div className="w-1 sm:h-80 h-40 bg-[#686C45]" />
@@ -28,22 +28,20 @@ const Hero = () => {
 			{/* <div className="w-full h-1/2 justify-end top-300"> */}
 			<MyLife />
 			{/* </div> */}
-			<div className="absolute xs:bottom-10 bottom-10 w-full flex justify-center items-center">
-				<a href="#about">
-					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
-						<motion.div
-							animate={{
-								y: [0, 24, 0],
-							}}
-							transition={{
-								duration: 1.5,
-								repeat: Infinity,
-								repeatType: "loop",
-							}}
-							className="w-3 h-3 rounded-full bg-white mb-1"
-						/>
-					</div>
-				</a>
+			<div className="absolute  bottom-20 w-full flex justify-center items-center">
+				<div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+					<motion.div
+						animate={{
+							y: [0, 24, 0],
+						}}
+						transition={{
+							duration: 1.5,
+							repeat: Infinity,
+							repeatType: "loop",
+						}}
+						className="w-3 h-3 rounded-full bg-white mb-1"
+					/>
+				</div>
 			</div>
 		</section>
 	);
