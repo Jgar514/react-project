22af96ee9a360a5c2f3b00096be954f22e83e698
Joshua Garvey <j.garvey514@yahoo.com>
2023-06-27 20:20:44 -0400

    contact form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ import { MyLife } from "./canvas";
 const Hero = () => {
 	return (
 		<section
-			className={`relative w-full h-screen mx-auto bg-black overlow-hidden
+			className={`relative w-full h-screen mx-auto bg-black overlow-hidden mt-4
 		`}
 		>
-			<div className={`${styles.paddingX} absolute inset-0 top-[430px] md:top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+			<div className={`${styles.paddingX} absolute inset-0 top-[410px] md:top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
 				<div className="flex flex-col justify-center items-center mt-5">
 					<div className="w-5 h-5 rounded-full bg-[#686C45]" />
 					<div className="w-1 sm:h-80 h-40 bg-[#686C45]" />
